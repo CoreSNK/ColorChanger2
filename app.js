d7e067bcfd0f8590f3cbd5b93ff2f147b875addf
CoreSNK <gnengel07@gmail.com>
2023-06-14 20:32:11 -0500

    Changed the color units to HSL

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-const CHANGER = document.querySelector("#changer");
-const RESET = document.querySelector("#reset");
-const FONTCHANGE = document.querySelector("#font");
-const COLORS = ["#23ac21", "#ff23cb", "#1423ac", "rgb(0,85,225)", "#ffccdd"];
-const FONTS = ["arial", "Montserrat", "Oswald", "roboto"];
-const hexColorLetter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const hexColorNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const btn = document.querySelector('#changer');
+const color = document.querySelector('.color');
 
+const setBg = () => {
+  const hue = Math.floor(Math.random() * 360);
+  const saturation = Math.floor(Math.random() * 100);
+  const lightness = Math.floor(Math.random() * 100);
+  const randomColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+  console.log(randomColor);
+}
 
-CHANGER.addEventListener("click", () => {
-  const randomIdx = Math.floor(Math.random() * COLORS.length);
-  document.body.style.background = COLORS[randomIdx];
-});
+changer.addEventListener('click', setBg);
+setBg();
 
-FONTCHANGE.addEventListener("click", () => {
-  const randomFont = Math.floor(Math.random() * FONTS.length);
-  document.body.style.fontFamily = FONTS[randomFont];
-});
-
-RESET.addEventListener("click", () => {
-  document.body.style.background = "#FFF";
-  document.body.style.fontFamily = "arial";
-});
